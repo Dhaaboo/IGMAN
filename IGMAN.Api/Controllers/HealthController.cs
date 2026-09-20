5835b89ec691be6bb12cad80a4cd0d4531e8bf32
Dhaaboo <xarwaashe00@gmail.com>
2026-09-20 14:34:49 +0300

    Api And Web Connection

---
+++ IGMAN.Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IGMAN.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "online",
+                service = "IGMAN.Api"
+            });
+        }
+    }
+}
--- IGMAN.Web/Program.cs
+++ IGMAN.Web/Program.cs
@@ -11,6 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient("IGMAN.Api", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7081");
+});
+
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
